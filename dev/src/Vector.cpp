041e1e4440b95597f57beed63015867ce738ba2e
Meculus (Gold Rebirth) <ethan.saff@gmail.com>
2017-03-16 15:24:44 -0400

    Created Vector class
    - First real functionality!
    - Created Vector class
    - Vectors can now be instantiated

---
+++ dev/src/Vector.cpp
@@ -0,0 +1,23 @@
+/*
+ * Vector.cpp
+ *
+ *  Created on: Mar 16, 2017
+ *      Author: sunlightiv
+ */
+
+#include "Vector.h"
+
+namespace phys {
+
+Vector::Vector(float x, float y, float z) {
+	// TODO Auto-generated constructor stub
+	this->x=x;
+	this->y=y;
+	this->z=z;
+}
+
+Vector::~Vector() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace phys */
---
+++ dev/src/Vector.h
@@ -0,0 +1,48 @@
+/*
+ * Vector.h
+ *
+ *  Created on: Mar 16, 2017
+ *      Author: sunlightiv
+ */
+
+#ifndef VECTOR_H_
+#define VECTOR_H_
+
+namespace phys {
+
+class Vector {
+public:
+	Vector(float x, float y, float z=0);
+	virtual ~Vector();
+
+	float getX() const {
+		return x;
+	}
+
+	void setX(float x) {
+		this->x = x;
+	}
+
+	float getY() const {
+		return y;
+	}
+
+	void setY(float y) {
+		this->y = y;
+	}
+
+	float getZ() const {
+		return z;
+	}
+
+	void setZ(float z) {
+		this->z = z;
+	}
+
+private:
+	float x, y, z;
+};
+
+} /* namespace phys */
+
+#endif /* VECTOR_H_ */
