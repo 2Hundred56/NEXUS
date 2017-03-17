e684eeb7b6e2c1596c552e569a57b8aca91a4100
Meculus (Gold Rebirth) <ethan.saff@gmail.com>
2017-03-17 09:27:19 -0400

    Invisible class
    - Added Invisible class for objects that wish to not be drawn
    (implements Drawable but does nothing when drawn)

---
+++ dev/src/Invisible.cpp
@@ -0,0 +1,24 @@
+/*
+ * Invisible.cpp
+ *
+ *  Created on: Mar 17, 2017
+ *      Author: sunlightiv
+ */
+
+#include "Invisible.h"
+#include "Canvas.h"
+namespace comps {
+
+Invisible::Invisible() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Invisible::~Invisible() {
+	// TODO Auto-generated destructor stub
+}
+void Invisible::draw(cnvs::Canvas) {
+
+}
+
+} /* namespace comps */
